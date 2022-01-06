36bf87cc1d19ba4df1de2187b0bd08214aa4e320
Matheus <matheuspsilva29@gmail.com>
2022-01-06 17:58:34 -0300

    Criando o card de listagem de registros

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+
+                <!-- Início do card de busca -->
+                <div class="card mb-3">
                     <div class="card-header">Busca de Marcas</div>
                     <div class="card-body">
                         <div class="form-row">
@@ -22,6 +24,49 @@
                         <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
                     </div>
                 </div>
+                <!-- Fim do card de busca -->
+
+                <!-- Início do card de listagem de marcas -->
+                <div class="card">
+                    <div class="card-header">Listagem de Marcas</div>
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer">
+                        <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
+                    </div>
+                </div>
+
+                <!-- Fim do card de listagem de marcas -->
+
             </div>
         </div>
     </div>
